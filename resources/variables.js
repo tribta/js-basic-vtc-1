adca750c5ad2d2025b25a4941c25dc83c52bba78
Gabriel7721 <176547965+tribta@users.noreply.github.com>
2025-06-17 18:45:08 +0700

    variables

---
+++ resources/variables.js
@@ -0,0 +1,34 @@
+// Variables
+// old
+// var myName = "Donald Trump";
+// console.log(myName);
+
+// new
+let myName;
+myName = "Donald Trump";
+console.log(myName);
+
+// forbidden
+// let if;
+// let for;
+// let else;
+// let function;
+// let 9999rose;
+// let first name;
+// let first-name;
+// let first_name;
+
+// camelCase
+// let firstName;
+
+// khai báo age
+let x = 30;
+let age = 30;
+
+// case insensitive
+let firstName;
+let FirstName;
+
+// multiple khai báo
+let lastName1, lastName2;
+let lastName3 = "Donald", lastName4 = "Trump";
