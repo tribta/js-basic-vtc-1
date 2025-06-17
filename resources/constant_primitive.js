a0e7cd7f569f652bfa41ca465df7d3011c1dddb5
Gabriel7721 <176547965+tribta@users.noreply.github.com>
2025-06-17 18:59:17 +0700

    constant and primitive

---
+++ resources/constant_primitive.js
@@ -0,0 +1,34 @@
+// Constant = const: "Hằng số" - không đổi.
+// let interestRate = 0.3; // float
+// interestRate = 1; // int
+// console.log(interestRate);
+
+// used const
+// const rate = 0.3;
+// //rate = 1; // TypeError: Assignment to constant variable.
+// console.log(rate);
+
+// Primitive Types - Kiểu dữ liệu nguyên thủy.
+
+// 1. string literal
+let myName = "Donald";
+
+// 2. number literal
+let myAge = 18;
+
+// 3. boolean
+let isMarriage = true;
+
+// 4. Undefined
+let myAddress;
+console.log(myAddress);
+let myHouse = undefined;
+console.log(myHouse);
+
+// 5. Null
+let selectorColor = null;
+console.log(selectorColor);
+selectorColor = "Red";
+console.log(selectorColor);
+selectorColor = null;
+console.log(selectorColor);
